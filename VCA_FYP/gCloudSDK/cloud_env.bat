d22160544cf516c40caf4f062ceb81a5755e7bce
StephenK <komolafs@tcd.ie>
2025-03-01 18:11:12 +0000

    Text to Speech Implemented (+other things)
    -Text to Speech Implemented[Takes agent response text, sends it to google TTS API, receives base64 byte array data and decodes data into Speech.wav audio file.]
    
    -Audio2Face utilised and funtional[Real-time facial animation shows on metahuman correctly. Need to find way to automate speech.wav playback every time file is updated/agent has something new to say]
    
    -Added idle animation for the metahumans body to replace A-pose.

--- VCA_FYP/gCloudSDK/cloud_env.bat
+++
@@ -1,7 +0,0 @@
-ECHO OFF
-CLS
-SET PATH=C:\Users\skomo\OneDrive\Documents\GitHub\4th-Year-FYP\gCloudSDK\google-cloud-sdk\bin;%PATH%;
-cd C:\Users\skomo\OneDrive\Documents\GitHub\4th-Year-FYP\gCloudSDK
-ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
-ECHO ---
-ECHO ON
